76ce65f6b8086f5edebc602f85aa11ae424f3e8a
jrnold <jeffrey.arnold@gmail.com>
2017-03-11 13:59:00 -0800

    changes to holdout functions
    - rename args to size to be consistent with sample_n/sample_frac
    - ensure full test coverage

--- R/holdout.R
+++ R/holdout.R
@@ -1,18 +1,18 @@
 #' Generate cross-validation test-training pairs
 #'
-#' \code{holdout_frac} splits the data so that proportion \code{p} is in test
-#' set and \code{1 - p} is in the training set. Likewise, \code{holdout_n}
-#' splits the data so that \code{n} elements are in the test set and the
+#' \code{holdout_frac} splits the data so that proportion \code{size} is in test
+#' set and \code{1 - size} is in the training set. Likewise, \code{holdout_n}
+#' splits the data so that \code{size} elements are in the test set and the
 #' remainder are in the training set.
 #'
 #'
 #' @param x A data frame
-#' @param p Fraction (proportion) of elements in the test set.
-#' @param n Number of elements in the test set.
+#' @param size For \code{holdout_n}, the number of elements in the test set.
+#'   For \code{holdout_frac}, the fraction of elements in test set.
 #' @param k Number of test/train splits to generate.
 #' @param shuffle If \code{TRUE}, the observations are randomly assigned to the
-#'   test and training sets. If \code{FALSE}, then the last \code{p} or
-#'   \code{n} of the observations are assigned to the test set, and
+#'   test and training sets. If \code{FALSE}, then the last \code{size}
+#'   elements are assigned to the test set, and
 #'   the remainder of the observations are assigned to the training set.
 #' @param stratify If \code{TRUE}, then test-train splits are within each
 #'   code group, so that the final test and train subsets have approximately
@@ -33,45 +33,50 @@ holdout_frac <- function(x, ...) {
   UseMethod("holdout_frac")
 }
 
-#' @rdname holdout_frac
-#' @export
-holdout_frac.data.frame <- function(x, p = 0.3, k = 1, shuffle = TRUE, ...) {
-  assert_that(is.number(p) && p >= 0 && p <= 1)
-  n <- round(p * length(x))
-  holdout_n(x, n = n, k = k, shuffle = shuffle)
-}
+# Note: I wanted to use the same names as sample_n and sample_frac in dplyr
+# for consistency
 
 #' @rdname holdout_frac
 #' @export
-holdout_frac.grouped_df <- function(x, p = p, k = k,
-                                    shuffle = TRUE,
+holdout_frac.data.frame <- function(x, size = 0.3, k = 1, shuffle = TRUE, ...) {
+  # TODO: should I allow for size %in% c(0, 1) which will give size 0
+  assert_that(is.number(size) && size >= 0 && size <= 1)
+  assert_that(is.number(k) && k >= 1)
+  assert_that(is.flag(shuffle))
+  res <- holdout_frac_(nrow(x), size = size, k = k, shuffle = shuffle)
+  to_crossv_df(res, x)
+}
+
+#' @rdname holdout_frac
+#' @importFrom dplyr n_groups
+#' @export
+holdout_frac.grouped_df <- function(x, size = 0.3, k = k, shuffle = TRUE,
                                     stratify = FALSE,
                                     ...) {
-  assert_that(is.number(p) && p >= 0 && p <= 1)
+  assert_that(is.numeric(size) && all(size >= 0) && all(size <= 1))
+  assert_that(is.number(k) && k >= 1)
+  assert_that(is.flag(shuffle))
   assert_that(is.flag(stratify))
   idx <- group_indices_lst(x)
   if (stratify) {
-    f <- function(g) {
-      n <- p * length(g)
-      mutate_(holdout_n_(length(g), n = n, k = k, shuffle = shuffle),
+    # allow for either constant of different heldout sizes per group
+    assert_that(length(size) %in% c(1, n_groups(x)))
+    if (length(size) == 1) size <- rep(size, n_groups(x))
+    f <- function(g, size) {
+      mutate_(holdout_frac_(length(g), size = size, k = k, shuffle = shuffle),
               train = ~ map(train, function(i) g[i]),
               test = ~ map(test, function(i) g[i]))
     }
-    res <- summarise_(group_by_(map_df(idx, f), ".id"),
+    res <- summarise_(group_by_(map2_df(idx, size, f), ".id"),
                       train = ~ list(flatten_int(train)),
                       test = ~ list(flatten_int(test)))
-    to_crossv_df(res, x)
+    res <- to_crossv_df(res, x)
   } else {
-    n <- round(p * length(idx))
-    res <- holdout_n(x, n = n, k = k, stratify = FALSE, ...)
+    assert_that(length(size) == 1)
+    n <- round(size * length(idx))
+    res <- holdout_n(x, size = n, k = k, stratify = FALSE, ...)
   }
-}
-
-#' @rdname holdout_frac
-#' @export
-holdout_frac.default <- function(x, p = 0.3, k = 1L, shuffle = TRUE, ...) {
-  n <- round(p * length(x))
-  holdout_n(x, n, k = k, shuffle = shuffle)
+  res[ , c("train", "test", ".id")]
 }
 
 #' @rdname holdout_frac
@@ -82,49 +87,66 @@ holdout_n <- function(x, ...) {
 
 #' @rdname holdout_frac
 #' @export
-holdout_n.data.frame <- function(x, n = 1L, k = 1L, shuffle = TRUE, ...) {
-  res <- holdout_n_(nrow(x), n = n, k = k, shuffle = shuffle, ...)
+holdout_n.data.frame <- function(x, size = 1L, k = 1L, shuffle = TRUE, ...) {
+  # TODO: should I check that size <= nrow(x) ? or just return size 0 sets
+  assert_that(is.number(size) && size >= 1)
+  assert_that(is.number(k) && k >= 1)
+  assert_that(is.flag(shuffle))
+  res <- holdout_n_(nrow(x), size = size, k = k, shuffle = shuffle)
   to_crossv_df(res, x)
 }
 
 #' @rdname holdout_frac
+#' @importFrom dplyr n_groups
 #' @export
-holdout_n.grouped_df <- function(x, n = 1L, k = 1L, shuffle = TRUE,
+holdout_n.grouped_df <- function(x, size = 1L, k = 1L, shuffle = TRUE,
                                  stratify = FALSE, ...) {
+  assert_that(is.numeric(size) && all(size >= 1))
+  size <- as.integer(size)
+  assert_that(is.number(k) && k >= 1)
+  assert_that(is.flag(shuffle))
   assert_that(is.flag(stratify))
   idx <- group_indices_lst(x)
   if (stratify) {
-    f <- function(g) {
-      mutate_(holdout_n_(length(g), n = n, k = k, shuffle = shuffle),
+    # allow for different sizes for each group
+    assert_that(length(size) %in% c(1L, n_groups(x)))
+    if (length(size) == 1) size <- rep(size, n_groups(x))
+    f <- function(g, size) {
+      mutate_(holdout_n_(length(g), size = size, k = k, shuffle = shuffle),
               train = ~ map(train, function(i) g[i]),
               test = ~ map(test, function(i) g[i]))
     }
-    res <- summarise_(group_by_(map_df(idx, f), ".id"),
+    res <- summarise_(group_by_(map2_df(idx, size, f), ".id"),
                       train = ~ list(flatten_int(train)),
                       test = ~ list(flatten_int(test)))
   } else {
-    res <- mutate_(holdout_n_(length(idx), n = n, k = k, shuffle = shuffle),
+    assert_that(length(size) == 1)
+    res <- mutate_(holdout_n_(length(idx), size = size, k = k,
+                              shuffle = shuffle),
                    train ~ map(train, function(i) flatten_int(idx[i])),
                    test ~ map(test, function(i) flatten_int(idx[i])))
   }
   to_crossv_df(res, x)[ , c("train", "test", ".id")]
 }
 
-holdout_n_ <- function(x, n = 1L, k = 1L, shuffle = TRUE) {
-  assert_that(is.number(x) && x >= 1)
-  assert_that(is.number(n) && n >= 0)
-  assert_that(is.number(n) && k >= 0)
-  assert_that(is.flag(shuffle))
+# using size for the name of the heldout set and n for the size of the
+# vector is confusing, but more consistent with the names of the other functions
+# x as the name as the first arg doesn't make sense if it is a single integer.
+holdout_n_ <- function(n, size = 1L, k = 1L, shuffle = TRUE) {
   f <- function(i) {
     if (shuffle) {
-      idx <- sample.int(x, x, replace = FALSE)
+      idx <- sample.int(n, n, replace = FALSE)
     } else {
-      idx <- seq_len(x)
+      idx <- seq_len(n)
     }
-    test_idx <- utils::tail(idx, n)
+    test_idx <- utils::tail(idx, size)
     tibble(train = list(setdiff(idx, test_idx)),
            test = list(test_idx),
            .id = i)
   }
   map_df(seq_len(k), f)
+}
+
+holdout_frac_ <- function(n, size = 0.3, k = 1L, shuffle = TRUE) {
+  holdout_n_(n, size = size * n, k = k, shuffle = shuffle)
 }
--- tests/testthat/test-holdout.R
+++ tests/testthat/test-holdout.R
@@ -1,31 +1,71 @@
-context("holdout_n")
-
 local({
+  context("holdout_n")
+
   dat <- tibble(a = c(rep("a", 5), rep("b", 3), rep("c", 2)))
   dat_grouped <- group_by(dat, a)
 
   test_that("holdout_n.data.frame works as expected", {
-    n <- 2
+    size <- 2
     k <- 5
-    x <- holdout_n(dat, n = n, k = k)
+    x <- holdout_n(dat, size = size, k = k)
     expect_crossv_df(x)
     expect_equal(nrow(x), k)
-    expect_true(all(map_int(x$test, ~ dim(.x)[1]) == n))
-    expect_true(all(map_int(x$train, ~ dim(.x)[1]) == (nrow(dat) - n)))
+    expect_true(all(map_int(x$test, ~ dim(.x)[1]) == size))
+    expect_true(all(map_int(x$train, ~ dim(.x)[1]) == (nrow(dat) - size)))
   })
 
   test_that("holdout_n.grouped_df works as expected", {
-    n <- 2
+    size <- 2
     k <- 5
-    x <- holdout_n(dat_grouped, n = n, k = k, stratify = FALSE)
+    x <- holdout_n(dat_grouped, size = size, k = k, stratify = FALSE)
     expect_crossv_df(x)
     expect_equal(nrow(x), k)
   })
 
   test_that("holdout_n.grouped_df stratify = TRUE works as expected", {
-    n <- 1
+    size <- 1
     k <- 5
-    x <- holdout_n(dat_grouped, n = n, k = k, stratify = TRUE)
+    x <- holdout_n(dat_grouped, size = size, k = k, stratify = TRUE)
+    expect_crossv_df(x)
+    expect_equal(nrow(x), k)
+  })
+
+  test_that("holdout_n.data.frame shuffle = FALSE works as expected", {
+    size <- 2
+    k <- 1
+    x <- holdout_n(dat, size = size, k = k, shuffle = FALSE)
+    expect_crossv_df(x)
+    expect_equal(nrow(x), k)
+    expect_identical(x$train[[1]], resample(dat, 1:8))
+    expect_identical(x$test[[1]], resample(dat, 9:10))
+  })
+
+  context("holdout_frac")
+
+  test_that("holdout_frac.data.frame works as expected", {
+    size <- 0.3
+    n <- nrow(dat)
+    heldout <- round(size * n)
+    k <- 2
+    x <- holdout_frac(dat, size = size, k = k)
+    expect_crossv_df(x)
+    expect_equal(nrow(x), k)
+    expect_true(all(map_int(x$test, ~ dim(.x)[1]) == heldout))
+    expect_true(all(map_int(x$train, ~ dim(.x)[1]) == (n - heldout)))
+  })
+
+  test_that("holdout_frac.grouped_df works as expected", {
+    size <- 0.3
+    k <- 2
+    x <- holdout_frac(dat_grouped, size = size, k = k, stratify = FALSE)
+    expect_crossv_df(x)
+    expect_equal(nrow(x), k)
+  })
+
+  test_that("holdout_frac.grouped_df stratify = TRUE works as expected", {
+    size <- 0.3
+    k <- 2
+    x <- holdout_frac(dat_grouped, size = size, k = k, stratify = TRUE)
     expect_crossv_df(x)
     expect_equal(nrow(x), k)
   })
